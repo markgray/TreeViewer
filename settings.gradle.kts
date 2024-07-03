21b4d9705f065f5eff2b972f418809d9ee344fee
markgray <mark.t.gray@gmail.com>
2024-07-03 04:56:47 -0400

    Converted build files to kts, and updated AGP to 8.5.0

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage") // TODO: Keep an eye on this
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+include(":app")
+rootProject.name = "Tree Viewer"
