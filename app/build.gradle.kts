22ac632fc987462146aaa5b88d21834f21bfdd29
markgray <mark.t.gray@gmail.com>
2025-06-06 06:18:17 -0400

    Updated AGP to 8.10.1, and updated gradle to 8.14.1 and updated gradle dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     testImplementation("junit:junit:4.13.2")
